81226a0a46d9b92d74dcd964d401f6ef14fcda98
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-19 14:55:27 +0530

    Create CustomerDto class

--- src/main/java/edu/icet/model/dto/CustomerDto.java
+++ src/main/java/edu/icet/model/dto/CustomerDto.java
@@ -1,4 +1,18 @@
 package edu.icet.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDto {
+    private String id;
+    private String name;
+    private String address;
+    private String phone;
+
 }
