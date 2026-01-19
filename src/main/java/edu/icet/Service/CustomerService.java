08f5b904d7eee403964f929adbe01b13caa93097
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-19 15:16:32 +0530

    Create method save for POST api

--- src/main/java/edu/icet/Service/CustomerService.java
+++ src/main/java/edu/icet/Service/CustomerService.java
@@ -1,7 +1,17 @@
 package edu.icet.Service;
 
+import edu.icet.Repository.CustomerRepository;
+import edu.icet.model.dto.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public void save(CustomerDto customerDto) {
+        customerRepository.save(customerDto);
+    }
 }
