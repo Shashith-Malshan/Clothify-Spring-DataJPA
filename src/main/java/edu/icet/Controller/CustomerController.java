fc0e727b14251e071c0870c56d32c4c53ceb18ff
Shashith-Malshan <malshanshashith318@gmail.com>
2026-01-19 15:16:12 +0530

    Create method save for POST api

--- src/main/java/edu/icet/Controller/CustomerController.java
+++ src/main/java/edu/icet/Controller/CustomerController.java
@@ -1,4 +1,24 @@
 package edu.icet.Controller;
 
+import edu.icet.Service.CustomerService;
+import edu.icet.model.dto.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customerInfo")
+
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping
+    public String save(@RequestBody CustomerDto customerDto){
+        customerService.save(customerDto);
+        return "Customer added successfully";
+    }
+
 }
